c27b87c94f68e761920f2a76f5f6b22b24f7967a
Stephen Taylor Witte <stevetwitte@gmail.com>
2018-05-30 16:43:52 -0400

    tag helper methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,14 @@ class Post < ApplicationRecord
 
   validates :user,
             presence: true
+
+  def set_tags(names)
+    self.tags = names.split(",").map do |name|
+      Tag.where(name: name.strip.downcase).first_or_create!
+    end
+  end
+
+  def add_tag(name)
+    self.tags << Tag.where(name: name.strip.downcase).first_or_create!
+  end
 end
