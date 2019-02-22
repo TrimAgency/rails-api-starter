76e783de0bea287f9e1b5878f51dccd94a695a34
Stephanie Ricardo <stephanie.ricardo1@gmail.com>
2019-02-21 22:27:00 -0500

    Removes secrets.yml.enc

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDISCLOUD_URL'] || 'redis://redis:6379' }
+    config.redis = { url: ENV['REDIS_URL'] || 'redis://redis:6379' }
 end
 
 Sidekiq.configure_client do |config|
-    config.redis = { size: 3, url: ENV['REDISCLOUD_URL'] || 'redis://redis:6379' }
+    config.redis = { size: 3, url: ENV['REDIS_URL'] || 'redis://redis:6379' }
 end
