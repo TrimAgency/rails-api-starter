220a74fc2589a631d56c5b91aaa316039cd28f05
Stephanie Ricardo <stephanie.ricardo1@gmail.com>
2019-07-06 21:40:16 -0400

    Completes user request spec

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,294 +1,319 @@
 require 'rails_helper'
 require 'swagger_helper'
 
+# NOTE: Extra comments added here for reference
 RSpec.describe UsersController, type: :request do
   include AuthHelper
   USER_ROOT= 'user'.freeze
 
   describe 'Users API' do
     let!(:user_one) { create(:user, :consumer_user) }
+    let!(:user_two) { create(:user, :consumer_user) }
+    let!(:taken_email) { create(:user, :consumer_user, email: 'test@test.com')  }
 
+    # Run swagger tests using it blocks
+    before(:each) do |example|
+      submit_request(example.metadata)
+    end
+
+    # Build response examples
+    after(:each) do |example|
+      # Swagger only allows one example per status code, skip extra tests if needed
+      unless example.metadata[:skip_swagger]
+        example.metadata[:response][:examples] = 
+          { 'application/json' => JSON.parse(response.body, symbolize_names: true) }
+      end
+    end
+
+    # SHOW
     path '/users/{id}' do
       get 'Retrieves a user' do
         tags 'Users'
+        consumes 'application/json'
         produces 'application/json'
-        security [Bearer: {}]
-        parameter name: :id, :in => :path, :type => :string
+        security [ Bearer: {} ]
+        parameter name: :id, in: :path, type: :string
 
-        response '200', 'user found' do
-          let(:"Authorization") { "Bearer #{token_for(user_one)}" }
+        response '200', 'User found' do
+          let!(:Authorization) { auth_headers_for(user_one) }
           schema type: :object,
             properties: {
               id: { type: :integer },
               email: { type: :string },
+              profile: { type: :object, 
+                properties: {
+                  id: { type: :integer },
+                  first_name:{ type: :string },
+                  last_name: { type: :string },
+                }
+              },
               profile_type: { type: :string }
-            }
+          }
+          let!(:id) { user_one.id }
 
-          let(:id) { user_one.id }
-
-          before do |example|
-            submit_request(example.metadata)
-          end
-
-          after do |example|
-            example.metadata[:response][:examples] = { 'application/json' => JSON.parse(response.body, symbolize_names: true) }
-          end
-
-          it 'responds with 200 OK' do |example|
+          it 'responds with 200 OK' do
             expect(response).to have_http_status :ok
           end
 
-          it 'returns expected attributes in valid JSON' do |example|
-            expect(response.body).to eql(UserSerializer.render(user_one, root: USER_ROOT, view: :show))
+          it 'returns expected attributes in valid JSON' do
+            expect(response.body).to eql(
+              UserSerializer.render(user_one, root: USER_ROOT, view: :show)
+            )
           end
         end
 
-        # response '404', 'user not found' do
-        #   let(:"Authorization") { "Bearer #{token_for(user_one)}" }
-        #   let(:id) { 'invalid' }
-        #   run_test!
-        # end
+        response '404', 'User not found' do
+          let!(:Authorization) { auth_headers_for(user_one) }
+          let!(:id) { 'invalid' }
 
-        # response '401', 'unauthorized' do
-        #   let(:"Authorization") { "Bearer " }
-        #   let(:id) { user_one.id }
-        #   run_test!
-        # end
+          it 'responds with 404 Not Found' do
+            expect(response).to have_http_status :not_found
+          end
+        end
+
+        response '401', 'Unauthorized' do
+          context 'when accessing another user\'s data' do
+            let!(:Authorization) { auth_headers_for(user_two) }
+            let!(:id) { user_one.id }
+
+            it 'responds with 401 unauthorized' do
+              expect(response).to have_http_status :unauthorized
+            end
+          end
+
+          context 'when logged out' do
+            let!(:Authorization) { non_auth_headers }
+            let!(:id) { user_one.id }
+  
+            it 'responds with 401 unauthorized' do
+              expect(response).to have_http_status :unauthorized
+            end
+          end
+        end  
+      end
+    end
+
+    # CREATE
+    path '/users' do
+      post 'Creates a user' do
+        tags 'Users'
+        consumes 'application/json'
+        produces 'application/json'
+        security [ Bearer: {} ]
+        parameter name: :user, in: :body, schema: {
+          type: :object,
+          properties: {
+            email: { type: :string },
+            password: { type: :string },
+            password_confirmation: { type: :string },
+            profile: { type: :object, 
+              properties: {
+                id: { type: :integer },
+                first_name:{ type: :string },
+                last_name: { type: :string },
+              }
+            },
+            profile_type: { type: :string }
+          }
+        }
+
+        let!(:valid_params) do
+          { email: Faker::Internet.email,
+            password: '123123123',
+            password_confirmation: '123123123',
+            profile_type: 'consumer',
+            profile: {
+                first_name: Faker::Name.first_name,
+                last_name: Faker::Name.last_name
+            } }
+        end
+        
+        let!(:missing_params) do
+          { email: Faker::Internet.email,
+            password: '123123123',
+            password_confirmation: '123123123',
+            profile_type: 'consumer',
+            profile: {
+                last_name: Faker::Name.last_name
+            } }
+        end
+
+        let(:taken_params) do
+          { email: 'test@test.com',
+            password: '123123123',
+            password_confirmation: '123123123',
+            profile_type: 'consumer',
+            profile: {
+                first_name: Faker::Name.first_name,
+                last_name: Faker::Name.last_name
+          } }
+        end
+        
+        let!(:invalid_params) do
+          { email: 'not@good',
+            password: '123123123',
+            password_confirmation: '123123123',
+            profile_type: 'consumer',
+            profile: {
+                first_name: Faker::Name.first_name,
+                last_name: Faker::Name.last_name
+            } }
+        end
+
+        response '201', 'User created' do
+          let!(:Authorization) { non_auth_headers }
+          schema type: :object,
+            properties: {
+              id: { type: :integer },
+              email: { type: :string },
+              profile: { type: :object, 
+                properties: {
+                  id: { type: :integer },
+                  first_name:{ type: :string },
+                  last_name: { type: :string },
+                }
+              },
+              profile_type: { type: :string }
+          }
+          let!(:user) { valid_params }
+
+          it 'responds with 201 Created' do
+            expect(response).to have_http_status :created
+          end
+
+          it 'successfully creates a user' do
+            expect(User.last.email).to eql valid_params[:email]
+          end
+
+          it 'returns expected attributes in valid JSON' do
+            token = JSON.parse(response.body)['user']['token']
+            expect(response.body).to include UserSerializer.render(User.last, 
+                                                                  root: USER_ROOT,
+                                                                  view: :create, 
+                                                                  token: token)
+          end
+        end
+
+        response '400', 'Unable to create user' do
+          let!(:Authorization) { non_auth_headers }
+          schema type: :object,
+            properties: {
+              id: { type: :integer },
+              email: { type: :string },
+              profile: { type: :object, 
+                properties: {
+                  id: { type: :integer },
+                  first_name:{ type: :string },
+                  last_name: { type: :string },
+                }
+              },
+              profile_type: { type: :string }
+          }
+
+          context 'with missing user data' do
+            let!(:user) { missing_params }
+
+            it 'responds with 400 Bad Request' do
+              expect(response).to have_http_status :bad_request
+            end
+    
+            it 'responds with errors' do
+              expect(response.body).to include 'errors', 'first_name'
+            end
+          end
+
+          context 'with email that is taken' do
+            let!(:user) { taken_params }
+
+            it 'responds with 400 Bad Request', :skip_swagger do
+              expect(response).to have_http_status :bad_request
+            end
+    
+            it 'responds with errors', :skip_swagger do
+              expect(response.body).to include 'errors', 'email'
+            end
+          end
+
+          context 'with invalid email' do
+            let!(:user) { invalid_params }
+
+            it 'responds with 400 Bad Request', :skip_swagger do
+              expect(response).to have_http_status :bad_request
+            end
+    
+            it 'responds with errors', :skip_swagger do
+              expect(response.body).to include 'errors', 'email'
+            end
+          end
+        end
+      end
+    end
+
+    # UPDATE
+    path '/users/{id}' do
+      patch 'Updates a user' do
+        tags 'Users'
+        consumes 'application/json'
+        produces 'application/json'
+        security [ Bearer: {} ]
+        parameter name: :id, in: :path, type: :string
+        parameter name: :user, in: :body, schema: {
+          type: :object,
+          properties: {
+            email: { type: :string },
+            password: { type: :string },
+            password_confirmation: { type: :string }
+          }
+        }
+
+        let!(:update_params) { { email: 'newemail@test.com' } }
+
+        response '200', 'User updated' do
+          let!(:Authorization) { auth_headers_for(user_one) }
+          schema type: :object,
+            properties: {
+              id: { type: :integer },
+              email: { type: :string },
+              profile: { type: :object, 
+                properties: {
+                  id: { type: :integer },
+                  first_name:{ type: :string },
+                  last_name: { type: :string },
+                }
+              },
+              profile_type: { type: :string }
+          }
+          let!(:id) { user_one.id }
+          let!(:user) { update_params }
+
+          it 'responds with 200 OK' do
+            expect(response).to have_http_status :ok
+          end
+          
+          it 'updates the record' do
+            expect(User.find(user_one.id).email).to eql 'newemail@test.com'
+          end
+          
+          it 'returns expected attributes in valid JSON' do
+            user = User.find(user_one.id)
+            expect(response.body).to eql(
+              UserSerializer.render(user, root: USER_ROOT, view: :update)
+            )
+          end
+        end
+
+        response '401', 'Unauthorized' do
+          context 'when updating another user\'s data' do
+            let!(:Authorization) { auth_headers_for(user_two) }
+            let!(:id) { user_one.id }
+            let!(:user) { update_params }
+
+            it 'responds with 401 unauthorized' do
+              expect(response).to have_http_status :unauthorized
+            end
+          end
+        end
       end
     end
   end
-
-  # describe 'get #show' do
-  #   context 'as a consumer' do
-  #     let!(:user_one) { create(:user, :consumer_user) }
-
-  #     let!(:user_two) { create(:user, :consumer_user) }
-
-  #     context 'logged in' do
-  #       context 'accessing their information' do
-  #         before do
-  #           get user_path(user_one),
-  #               headers: auth_headers_for(user_one),
-  #               as: :json
-  #         end
-
-  #         it 'responds with 200 OK' do
-  #           expect(response).to have_http_status :ok
-  #         end
-
-  #         it 'returns expected attributes in valid JSON' do
-  #           expect(response.body).to eql({ user: UserSerializer.new(user_one).attributes }.to_json)
-  #         end
-  #       end
-
-  #       context 'accessing another users information' do
-  #         before do
-  #           get user_path(user_two),
-  #               headers: auth_headers_for(user_one),
-  #               as: :json
-  #         end
-
-  #         it 'responds with 401 Unauthorized' do
-  #           expect(response).to have_http_status :unauthorized
-  #         end
-  #       end
-
-  #     end
-
-  #     context 'logged out' do
-  #       context 'accessing any users information' do
-  #         before do
-  #           get user_path(user_one),
-  #               headers: non_auth_headers,
-  #               as: :json
-  #         end
-
-  #         it 'responds with 401 Unauthorized' do
-  #           expect(response).to have_http_status :unauthorized
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
-
-  # describe 'post #create' do
-  #   context 'as a consumer' do
-  #     let(:valid_params) do
-  #       { 
-  #         email: Faker::Internet.email,
-  #         password: '123123123',
-  #         password_confirmation: '123123123',
-  #         profile_type: 'consumer',
-  #         profile: {
-  #             first_name: Faker::Name.first_name,
-  #             last_name: Faker::Name.last_name
-  #         }
-  #       }
-  #     end
-
-  #     let(:missing_params) do
-  #       {
-  #         email: Faker::Internet.email,
-  #         password: '123123123',
-  #         password_confirmation: '123123123',
-  #         profile_type: 'consumer',
-  #         profile: {
-  #             last_name: Faker::Name.last_name
-  #         }
-  #       }
-  #     end
-
-  #     let(:invalid_params) do
-  #       {
-  #         email: 'not@good',
-  #         password: '123123123',
-  #         password_confirmation: '123123123',
-  #         profile_type: 'consumer',
-  #         profile: {
-  #             first_name: Faker::Name.first_name,
-  #             last_name: Faker::Name.last_name
-  #         }
-  #       }
-  #     end
-
-  #     context 'with valid user data' do
-  #       before do
-  #         post users_path,
-  #              params: valid_params,
-  #              headers: non_auth_headers,
-  #              as: :json
-  #       end
-
-  #       it 'responds with 201 Created' do
-  #         expect(response).to have_http_status :created
-  #       end
-
-  #       it 'successfully creates a user' do
-  #         expect(User.last.email).to eql valid_params[:email]
-  #       end
-
-  #       it 'returns expected attributes in valid JSON' do
-  #         expect(response.body).to include UserSerializer.new(User.last).attributes.to_json
-  #       end
-  #     end
-
-  #     context 'with missing user data' do
-  #       before do
-  #         post users_path,
-  #              params: missing_params,
-  #              headers: non_auth_headers,
-  #              as: :json
-  #       end
-
-  #       it 'responds with 400 Bad Request' do
-  #         expect(response).to have_http_status :bad_request
-  #       end
-
-  #       it 'responds with errors' do
-  #         expect(response.body).to include 'errors', 'first_name'
-  #       end
-  #     end
-
-  #     context 'with email that is taken' do
-  #       let(:taken_params) do
-  #         {
-  #           email: 'test@test.com',
-  #           password: '123123123',
-  #           password_confirmation: '123123123',
-  #           profile_type: 'consumer',
-  #           profile: {
-  #               first_name: Faker::Name.first_name,
-  #               last_name: Faker::Name.last_name
-  #           }
-  #         }
-  #       end
-
-  #       before do
-  #         create :user, email: 'test@test.com'
-
-  #         post users_path,
-  #              params: taken_params,
-  #              headers: non_auth_headers,
-  #              as: :json
-  #       end
-
-  #       it 'responds with 400 Bad Request' do
-  #         expect(response).to have_http_status :bad_request
-  #       end
-
-  #       it 'responds with errors' do
-  #         expect(response.body).to include 'errors', 'email'
-  #       end
-  #     end
-
-  #     context 'with invalid email' do
-  #       before do
-  #         post users_path,
-  #              params: invalid_params,
-  #              headers: non_auth_headers,
-  #              as: :json
-  #       end
-
-  #       it 'responds with 400 Bad Request' do
-  #         expect(response).to have_http_status :bad_request
-  #       end
-
-  #       it 'responds with errors' do
-  #         expect(response.body).to include 'errors', 'email'
-  #       end
-  #     end
-  #   end
-  # end
-
-  # describe 'patch #update' do
-  #   context 'as a consumer' do
-  #     let!(:user_one) { create(:user, :consumer_user) }
-
-  #     let!(:user_two) { create(:user, :consumer_user) }
-
-  #     let(:update_params) do
-  #       { 
-  #         email: 'newemail@test.com' 
-  #       }
-  #     end
-
-  #     context 'logged in' do
-  #       context 'updating their information' do
-  #         before do
-  #           patch user_path(user_one),
-  #                 params: update_params,
-  #                 headers: auth_headers_for(user_one),
-  #                 as: :json
-  #         end
-
-  #         it 'responds with 200 OK' do
-  #           expect(response).to have_http_status :ok
-  #         end
-
-  #         it 'updates the record' do
-  #           expect(User.find(user_one.id).email).to eql 'newemail@test.com'
-  #         end
-
-  #         it 'returns expected attributes in valid JSON' do
-  #           expect(response.body).to eql({ user: UserSerializer.new(User.find(user_one.id)).attributes }.to_json)
-  #         end
-  #       end
-  #     end
-
-  #     context 'logged in' do
-  #       context 'updating another users information' do
-  #         before do
-  #           patch user_path(user_two),
-  #                 params: update_params,
-  #                 headers: auth_headers_for(user_one),
-  #                 as: :json
-  #         end
-
-  #         it 'responds with 401 Unauthorized' do
-  #           expect(response).to have_http_status :unauthorized
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
 end
