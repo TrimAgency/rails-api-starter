a123912b5a16e519c4bf80811ef255df812ea6ab
Stephanie Ricardo <stephanie.ricardo1@gmail.com>
2019-08-23 12:11:25 -0400

    Updates controllers and mailers

---
+++ app/controllers/api/v1/consumers_controller.rb
@@ -0,0 +1,22 @@
+module Api
+  module V1
+    class ConsumersController < ApiController
+      CONSUMER_ROOT = 'consumer'.freeze
+      load_and_authorize_resource
+
+      def update
+        @consumer.update!(update_params)
+
+        render json: ConsumerSerializer.render(@consumer, root: CONSUMER_ROOT), 
+              status: :ok
+      end
+
+      private
+
+      def update_params
+        params.require(:consumer).permit(:first_name,
+                                         :last_name)
+      end
+    end
+  end
+end
